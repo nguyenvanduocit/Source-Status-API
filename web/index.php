fbd23dadd6f65fb91974e309fa216c388b99ee29
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-02 00:00:45 +0700

    integrate with heroku

--- web/index.php
+++ web/index.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app->register(new Silex\Provider\ValidatorServiceProvider());
 $app->post( '/translate', '\SlackBotService\Controller\Translate::translate' );
 $app->post( '/zingmp3', '\SlackBotService\Controller\ZingMp3::post' );
 $app->error( function ( \Exception $e, $code ) {
