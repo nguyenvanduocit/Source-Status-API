b132ddc55628a0817a293d7f24d82aada9cb9a64
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-07 14:09:59 +0700

    implement meme

--- web/Controller/Meme.php
+++ web/Controller/Meme.php
@@ -60,7 +60,7 @@ class Meme {
 			$backgroundPath = $this->memeList[$backgroundId];
 			$imagine = new Imagine();
 			$imagine->open($backgroundPath['src'])->save($this->outputDir.'/example.jpg');
-			$resultObject->setMessage($this->outputDir.'/example.jpg');
+			$resultObject->setMessage($request->getBasePath().'/Public/Meme/example.jpg');
 			return new JsonResponse($resultObject);
 		}
 		else{
