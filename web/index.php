a5a8c5ba4a43cf9f00dce116d1719de90a674c63
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-07 14:15:16 +0700

    implement meme

--- web/index.php
+++ web/index.php
@@ -11,8 +11,18 @@ $app = new Silex\Application();
 $app->post( '/translate', '\SlackBotService\Controller\Translate::translate' );
 $app->post( '/zingmp3', '\SlackBotService\Controller\ZingMp3::post' );
 $app->post( '/meme', '\SlackBotService\Controller\Meme::generate' );
+/**
+ * Error handler
+ */
 $app->error( function ( \Exception $e, $code ) {
 	return new JsonResponse( array( 'errorCode' => $code, 'message' => $e->getMessage() ) );
 } );
+/**
+ * Static file handler
+ */
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (is_file($filename)) {
+	return false;
+}
 
 $app->run();
