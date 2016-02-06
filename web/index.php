d7ba907b1687c1da5067db1bbfb72fafbb6d6019
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-02-06 19:24:14 +0700

    as

--- web/index.php
+++ web/index.php
@@ -6,17 +6,6 @@ require_once APP_DIR . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-//handling CORS preflight request
-$app->before(function (Request $request) {
-	if ($request->getMethod() === "OPTIONS") {
-		$response = new Response();
-		$response->headers->set("Access-Control-Allow-Origin","*");
-		$response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-		$response->headers->set("Access-Control-Allow-Headers","Content-Type");
-		$response->setStatusCode(200);
-		return $response->send();
-	}
-}, Application::EARLY_EVENT);
 //handling CORS respons with right headers
 $app->after(function (Request $request, Response $response) {
 	$response->headers->set("Access-Control-Allow-Origin","*");
